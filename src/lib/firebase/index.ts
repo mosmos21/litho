7705366c610dac81fdf9fe124e132283487d98e5
mosmos21 <dev@mosmos21.net>
2023-06-13 22:59:26 +0900

    firebaseのための設定を追加する

---
+++ src/lib/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmkkhEs4PXk5DHXSueLFsjBJoRY8AzOMU",
+  authDomain: "ritho-ae2e3.firebaseapp.com",
+  projectId: "ritho-ae2e3",
+  storageBucket: "ritho-ae2e3.appspot.com",
+  messagingSenderId: "655302590442",
+  appId: "1:655302590442:web:5f3cf60d46276f0ec8fc93",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
